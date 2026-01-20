64a24f74ddcf6334c63fc7c86d6049b5644b258b
khushijha24 <khushiujan2005@gmail.com>
2026-01-20 23:12:39 +0530

    initial commit

---
+++ src/main/java/queue/Main.java
@@ -0,0 +1,11 @@
+package queue;
+
+public class Main {
+
+    public static void main(String []args){
+        Queue myQueue = new Queue(4);
+
+        myQueue.printAll();
+
+    }
+}
---
+++ src/main/java/stack/Main.java
@@ -0,0 +1,10 @@
+package stack;
+
+public class Main {
+    public static void main(String []args){
+
+        Stack myStack = new Stack(4);
+
+        myStack.printAll();
+    }
+}
