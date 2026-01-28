4e34baef1b7b5652acabffcb809d5b1e56edab13
khushijha24 <khushiujan2005@gmail.com>
2026-01-28 10:02:12 +0530

    stack interview questions

--- src/main/java/stack/Main.java
+++ src/main/java/stack/Main.java
@@ -8,3 +8,27 @@ public class Main {
         myStack.printAll();
     }
 }
+
+/*
+public class Main {
+
+    public static void main(String[] args) {
+        Stack<Integer> stack;
+
+        // Test 1: Push single element
+        System.out.println("Test 1: Push Single Element");
+        stack = new Stack<>();
+        stack.push(10);
+        System.out.println("Expected Stack (top to bottom): 10");
+        System.out.println("Actual Stack:");
+        stack.printStack();
+        System.out.println("Peek: " + stack.peek());
+        System.out.println("Size: " + stack.size());
+        System.out.println();
+
+    }
+
+    }
+
+ */
+
