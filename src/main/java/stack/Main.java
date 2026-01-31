810316dee6472eabb527ffb3ef5c072b58de7f0a
khushijha24 <khushiujan2005@gmail.com>
2026-01-31 20:22:58 +0530

    reverseString and balancedParenthesis using stack

--- src/main/java/stack/Main.java
+++ src/main/java/stack/Main.java
@@ -1,11 +1,56 @@
 package stack;
 
 public class Main {
+
+    public static String reverseString (String string){
+
+        Stack<Character> stack = new Stack<>();
+        String reverseString = "";
+
+        for(char ch: string.toCharArray()){
+            stack.push(ch);
+        }
+
+        while (!stack.isEmpty()){
+            reverseString += stack.pop();
+        }
+        return reverseString;
+    }
+
+    public static boolean isBalancedParentheses(String string){
+        Stack<Character> stack =  new Stack<>();
+
+        // char[] charArray  = string.toCharArray();
+        // if(charArray[0] == ')') return false;
+
+        for(char ch: string.toCharArray()){
+
+            if(ch == '('){
+                stack.push(ch);
+            } else if(ch == ')'){
+                if(stack.isEmpty()|| stack.pop() != '('){
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
     public static void main(String []args){
 
-        Stack myStack = new Stack(4);
+        // reverse String
+//        System.out.println("Test 1: Normal Word");
+//        System.out.println("Input: 'hello'");
+//        System.out.println("Expected: 'olleh'");
+//        System.out.println("Actual: '" + reverseString("hello") + "'");
+//        System.out.println();
 
-        myStack.printAll();
+        // balanced parenthesis
+        System.out.println("Test 2: Single Pair");
+        System.out.println("Input: '()'");
+        System.out.println("Expected: true");
+        System.out.println("Actual: " + isBalancedParentheses("()"));
+        System.out.println();
     }
 }
 
--- src/main/java/stack/Stack.java
+++ src/main/java/stack/Stack.java
@@ -1,76 +1,76 @@
 package stack;
 
-public class Stack {
-    private Node top;
-    private int height;
+//public class Stack {
+//    private Node top;
+//    private int height;
+//
+//    class Node{
+//        int value;
+//        Node next;
+//
+//        Node(int value){
+//            this.value = value;
+//        }
+//    }
+//
+//    public Stack(int value){
+//        Node newNode = new Node(value);
+//        top = newNode;
+//        height = 1;
+//    }
+//
+//
+//    // Methods
+//    public Node getTop() {
+//        return top;
+//    }
+//
+//    public int getHeight() {
+//        return height;
+//    }
+//
+//    public void printStack() {
+//        Node temp = top;
+//        while (temp != null) {
+//            System.out.println(temp.value);
+//            temp = temp.next;
+//        }
+//    }
+//
+//    public void printAll() {
+//        if (height == 0) {
+//            System.out.println("Top: null");
+//        } else {
+//            System.out.println("Top: " + top.value);
+//        }
+//        System.out.println("Height:" + height);
+//        System.out.println("\nStack:");
+//        if (height == 0) {
+//            System.out.println("empty");
+//        } else {
+//            printStack();
+//        }
+//    }
+//
+//    public void makeEmpty() {
+//        top = null;
+//        height = 0;
+//    }
+//
+//    public void push(int value){
+//        Node newNode = new Node(value);
+//        if(top == null){
+//            top=newNode;
+//        }else{
+//            newNode.next = top;
+//            top = newNode;
+//        }
+//        height++;
+//    }
+//}
 
-    class Node{
-        int value;
-        Node next;
-
-        Node(int value){
-            this.value = value;
-        }
-    }
-
-    public Stack(int value){
-        Node newNode = new Node(value);
-        top = newNode;
-        height = 1;
-    }
 
 
-    // Methods
-    public Node getTop() {
-        return top;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void printStack() {
-        Node temp = top;
-        while (temp != null) {
-            System.out.println(temp.value);
-            temp = temp.next;
-        }
-    }
-
-    public void printAll() {
-        if (height == 0) {
-            System.out.println("Top: null");
-        } else {
-            System.out.println("Top: " + top.value);
-        }
-        System.out.println("Height:" + height);
-        System.out.println("\nStack:");
-        if (height == 0) {
-            System.out.println("empty");
-        } else {
-            printStack();
-        }
-    }
-
-    public void makeEmpty() {
-        top = null;
-        height = 0;
-    }
-
-    public void push(int value){
-        Node newNode = new Node(value);
-        if(top == null){
-            top=newNode;
-        }else{
-            newNode.next = top;
-            top = newNode;
-        }
-        height++;
-    }
-}
-
-
-/*
 
 
 import java.util.ArrayList;
@@ -119,4 +119,3 @@ public class Stack<T> {
     }
 
 }
-*/
